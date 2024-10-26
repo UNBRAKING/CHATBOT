d4ecfbb1d5cac0361bc412b5d5c0f925501020f6
UNBRAKING THINGS <code.pbail@gmail.com>
2024-10-26 22:28:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 API_ID = "26614080"
 # -------------------------------------------------------------
-API_HASH = "7512613727:AAE4bmjZuGDEOZvUP_rIOYxz1yLEle6eEUo"
+API_HASH = "7d2c9a5628814e1430b30a1f0dc0165b"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("7594693248:AAHubu-qceibM25E03dicbwoxvvppEUWhYA", None)
 MONGO_URL = getenv("mongodb+srv://nsdh77ygz5:b0zdFJkd9frHADgT@cluster0.vvof2.mongodb.net/?retryWrites=true&w=majority", None)
