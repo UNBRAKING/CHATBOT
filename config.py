a6c62fac8cf68c69fffbdc9875906460ea462ac9
UNBRAKING THINGS <code.pbail@gmail.com>
2024-10-26 22:57:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ API_ID = "26614080"
 API_HASH = "7d2c9a5628814e1430b30a1f0dc0165b"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("7594693248:AAHubu-qceibM25E03dicbwoxvvppEUWhYA", None)
-MONGO_URL = getenv("mongodb+srv://nsdh77ygz5:b0zdFJkd9frHADgT@cluster0.vvof2.mongodb.net/?retryWrites=true&w=majority", None)
+MONGO_URL = getenv("mongodb+srv://bohov18753:Sl7pDwSdy3l7uQpX@cluster0.qjohl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", None)
 OWNER_ID = int(getenv("OWNER_ID", "5606990991"))
 SUPPORT_GRP = "Team_Pbail"
 UPDATE_CHNL = "Ps_Support_Group"
