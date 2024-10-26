d6cf8a56dd4badf9eb6d78f86a5ed5ad77a3d8b8
UNBRAKING THINGS <code.pbail@gmail.com>
2024-10-26 21:16:15 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = "26614080"
+# -------------------------------------------------------------
+API_HASH = "7512613727:AAE4bmjZuGDEOZvUP_rIOYxz1yLEle6eEUo"
+# --------------------------------------------------------------
+BOT_TOKEN = getenv("7594693248:AAHubu-qceibM25E03dicbwoxvvppEUWhYA", None)
+MONGO_URL = getenv("mongodb+srv://nsdh77ygz5:b0zdFJkd9frHADgT@cluster0.vvof2.mongodb.net/?retryWrites=true&w=majority", None)
+OWNER_ID = int(getenv("OWNER_ID", "5606990991"))
+SUPPORT_GRP = "Team_Pbail"
+UPDATE_CHNL = "Ps_Support_Group"
+OWNER_USERNAME = "Pbail"
